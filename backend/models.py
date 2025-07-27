6566ada71342ff6d159a94691300e645daad6a78
robert <robertashe2.0@gmail.com>
2025-07-27 13:03:04 -0700

    Fix comment path for password hashing in User model

--- backend/models.py
+++ backend/models.py
@@ -7,7 +7,7 @@ db = SQLAlchemy()  # This is the tool our app uses to interact with the database
 class User(UserMixin, db.Model):
     __tablename__ = 'user'
     username = db.Column(db.String(64), primary_key=True)  # PRIMARY KEY
-    password = db.Column(db.String(128), nullable=False)   # stored as a hash in the database. Hashing/decoding takes place in auth.py
+    password = db.Column(db.String(128), nullable=False)   # stored as a hash in the database. Hashing/decoding takes place in backend/routes/auth.py
     firstName = db.Column(db.String(64), nullable=False)
     lastName = db.Column(db.String(64), nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)  # email MUST BE UNIQUE to prevent multiple accounts being registered to the same email
